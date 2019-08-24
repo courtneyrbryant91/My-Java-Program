f0d72165442d5a78d6a2a0ce6d97e0cf699a7306
courtneyrbryant91 <54428116+courtneyrbryant91@users.noreply.github.com>
2019-08-23 22:02:24 -0500

    Add files via upload

---
+++ prog2a.java
@@ -0,0 +1,38 @@
+/* Courtney Bryant
+ * Jordan-Matthews High
+ * Program 2a
+ * Created August 31, 2008
+ * Description: This program is used to teach how to use escape sequences in order to make programming simpler.
+ * What I learned: I learned that the escape sequence for \"return\" must use the backwards slash, not the forward slash.
+ */
+
+/**
+ Use this program to practice adding, deleting and 
+ changing lines in your IDE.
+ 
+ You will open he project called prog2a from 
+ the student master directory, make corrections, 
+ and then save the corrected program on your own diskette.
+ Finally run the program to verify the required changes 
+ were successfully completed.
+ 
+ @author gary langner
+ @version 1.0 November 2001
+ **/
+public class prog2a
+{
+    public static void main (String[] args)
+    {
+        System.out.println("The program you\nare using is\ncalled JAVA.  It\nwill be your friend.\nYou must treat it\nvery carefully.");
+        
+    }
+}
+
+/* Output:
+The program you
+are using is
+called JAVA.  It
+will be your friend.
+You must treat it
+very carefully.
+*/
